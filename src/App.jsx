406d666f7cc6626b4aca8475346844f217bb311d
Abdalkader Kouhda <abdalkaderkoadah2052002@gmail.com>
2024-10-23 23:13:24 +0300

    fixed errors in leetcode solved problems count

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,21 @@ import ProjectSection from './components/ProjectSection'
 import ContactUs from './components/ContactUs';
 import ServiceSection from './components/ServiceSection.jsx';
 import AwardsCertificates from './components/AwardsSection.jsx';
-import {data} from './data/data.js';
+import { data } from './data/data.js';
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <div className=" mx-auto">
-        <HeroSecion />
-        <ServiceSection />
-        <ProjectSection  projects={data.projects}/>
-        <AwardsCertificates/>
-        <ContactUs />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <div className=" mx-auto">
+                <HeroSecion />
+                <ServiceSection />
+                <ProjectSection projects={data.projects} />
+                <AwardsCertificates />
+                <ContactUs />
+            </div>
+        </>
+    );
 }
 
 export default App;
